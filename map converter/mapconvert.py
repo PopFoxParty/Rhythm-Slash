7ffd3c591b504c426894f031431bb44a9fd79d70
PopFoxParty <j9tsuna@gmail.com>
2022-06-10 09:21:49 -0400

    Added a python script to automatically convert .osu files into maps. You still need to manually import the .mp3.

---
+++ map converter/mapconvert.py
@@ -0,0 +1,26 @@
+# HOW TO USE: Import hitobjects from osu file into test.txt. Then, remove any text in outtext.txt. Run program to get map in outtext.txt.
+import sys
+try:
+    droppedFile = sys.argv[1]
+    mapName = input("Enter map name (no file extension): ")
+    songName = input("Enter song name (include file extension): ")
+    timeDelay = input("Enter note speed (how long the note will be on the screen for) (in ms): ")
+
+    with open (f"{mapName}.txt", "w") as songMapOut:
+        songMapOut.write(f"{songName} {timeDelay}\n")
+        with open(droppedFile, "r") as songMap:
+            lines = songMap.read().splitlines()
+            start = lines.index("[HitObjects]")
+            for line in lines[start+1:]:
+                if line.split(",")[4] in {"0", "4"}:
+                    noteColour = 2
+                elif line.split(",")[4] in {"8", "12"}:
+                    noteColour = 1
+                else:
+                    noteColour = 0
+                timing = line.split(",")[2]
+                songMapOut.write(f"{noteColour}  {timing}\n") 
+except IndexError:
+    print("No file dropped! Please drag and drop an .osu file onto the python script to convert it!")
+
+
