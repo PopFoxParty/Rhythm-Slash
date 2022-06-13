b27df5b23366f22c336ba9d89352da4f83c348aa
PopFoxParty <j9tsuna@gmail.com>
2022-06-13 16:24:00 -0400

    Fixed map conversion.

--- map converter/mapconvert.py
+++ map converter/mapconvert.py
@@ -6,7 +6,7 @@ try:
     songName = input("Enter song name (include file extension): ")
     timeDelay = input("Enter note speed (how long the note will be on the screen for) (in ms): ")
     with open (f"{mapName}.txt", "w") as songMapOut:
-        songMapOut.write(f"{songName} {timeDelay}\n")
+        songMapOut.write(f"\"{songName}\" {timeDelay}\n")
         try:
             with open(droppedFile, "r", encoding="utf-8") as songMap:
                 lines = songMap.read().splitlines()
@@ -14,7 +14,7 @@ try:
                 for line in lines[start+1:]:
                     if line.split(",")[4] in {"0", "4"}:
                         noteColour = 2
-                    elif line.split(",")[4] in {"8", "12"}:
+                    elif line.split(",")[4] in {"2", "8", "12"}:
                         noteColour = 1
                     else:
                         noteColour = 0
